88c0ef583dd17f74ddb746d8b6b2391054dc9b05
Toru <toorumon@gmail.com>
2019-01-10 16:16:29 -0800

    added loss function

--- nem.py
+++ nem.py
@@ -152,7 +152,15 @@ def compute_bernoulli_prior():
 	return torch.zeros(1, 1, 1, 1, 1)
 
 
-def nem_iterations(input_data, target_data, learning_rate=0.001, num_epochs=500, k=5, is_training=True):
+def compute_outer_loss(mu, gamma, target, prior, loss_inter_weigh=1.0):
+	raise NotImplementedError
+
+
+def compute_outer_ub_loss(pred, target, prior, loss_inter_weight=1.0):
+	raise NotImplementedError
+
+
+def nem_iterations(input_data, target_data, learning_rate=0.001, nr_steps=30, num_epochs=500, k=5, is_training=True):
 	# get input dimensions
 	input_shape = input_data.size()
 	assert input_shape[0] == 6, "Requires 6D input (T, B, K, W, H, C) but {}".format(input_shape[0])
@@ -187,18 +195,44 @@ def nem_iterations(input_data, target_data, learning_rate=0.001, num_epochs=500,
 	r_other_losses = []
 	r_other_ub_losses = []
 
+	outputs = [hidden_state]
+
 	for epoch in range(num_epochs):
 		if is_training:
 			model.train()
 
-		losses = 0.
+		losses = 0.0
 
-		for i, data in enumerate(input_data):
+		for t in range(nr_steps):
+			# model should predict the next frame
+			inputs = (input_data[t], target_data[t+1])
 
 			# forward pass
-			outputs = nem_model(data)
-			total_loss = intra_criterion(outputs, labels) + inter_criterion(outputs, labels)
+			hidden_state, output = nem_model(inputs, hidden_state)
+			theta, pred, gamma = output
+
+			# compute NEM losses
+			total_loss, intra_loss, inter_loss, r_total_loss, r_intra_loss, r_inter_loss \
+				= compute_outer_loss(pred, gamma, target_data[t+1], prior)
+
+			total_ub_loss, intra_ub_loss, inter_ub_loss, r_total_ub_loss, r_intra_ub_loss, r_inter_ub_loss \
+				= compute_outer_ub_loss(pred, target_data[t+1], prior)
+
+
+			total_loss = intra_criterion(output, labels) + inter_criterion(output, labels)
 			losses += total_loss
+
+			total_losses.append(total_loss)
+			total_ub_losses.append(total_ub_loss)
+
+			r_total_losses.append(r_total_loss)
+			r_total_ub_losses.append(r_total_ub_loss)
+
+			other_losses.append(torch.stack((total_loss, intra_loss, inter_loss)))
+			other_ub_losses.append(torch.stack((total_ub_loss, intra_ub_loss, inter_ub_loss)))
+
+			r_other_losses.append(torch.stack((r_total_loss, r_intra_loss, r_inter_loss)))
+			r_other_ub_losses.append(torch.stack((r_total_ub_loss, r_intra_ub_loss, r_inter_ub_loss)))
 
 			# backward pass and optimize
 			optimizer.zero_grad()
@@ -215,7 +249,9 @@ def nem_iterations(input_data, target_data, learning_rate=0.001, num_epochs=500,
               (phase, epoch, args.n_epoch, losses, best_valid_loss))
 
 	thetas, preds, gammas = zip(*outputs)
-	thetas
+	thetas = torch.stack((thetas,))
+	preds = torch.stack((preds,))
+	gammas = torch.stack((gammas,))
 
 	return loss, ub_loss, r_loss, r_ub_loss, thetas, preds, gammas, other_losses, other_ub_losses,\
 		 r_other_losses, r_other_ub_losses
@@ -237,15 +273,20 @@ def main(data_name, log_dir, nr_steps, batch_size, lr, max_epoch, noise_type='bi
 
 	# training
 	features_corrupted = add_noise(train_inputs['features'], noise_type=noise_type)
-	loss, ub_loss, r_loss, r_ub_loss, thetas, preds, gammas, other_losses, other_ub_losses, r_other_losses, \
-    r_other_ub_losses = nem_iterations(features_corrupted, features, learning_rate=lr, num_epochs=max_epoch)
+
+	loss, ub_loss, r_loss, r_ub_loss, thetas, preds, gammas, other_losses, other_ub_losses,\
+	r_other_losses, r_other_ub_losses = nem_iterations(features_corrupted, 
+												    	features, 
+												    	learning_rate=lr, 
+												    	nr_steps=nr_steps, 
+												    	num_epochs=max_epoch)
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--data_name' type=str, default='balls3curtain64')
 	parser.add_argument('--log_dir', type=str, default='./debug')
-	parser.add_argument('--nr_steps', type=int, default=100)
+	parser.add_argument('--nr_steps', type=int, default=30)
 	parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--max_epoch', type=int, default=500)
